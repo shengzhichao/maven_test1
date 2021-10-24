64feaf3a714cdc93c3280379b1e969823e0efddb
szc <iszc@qq.com>
2021-10-24 10:14:46 +0800

    third commit

--- src/main/java/com/szc/git/Hellogit.java
+++ src/main/java/com/szc/git/Hellogit.java
@@ -9,5 +9,8 @@ public class Hellogit {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
     }
 }
