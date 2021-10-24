c750668db5e3f74dc24f5d9890bbaefcd6d575dd
szc <iszc@qq.com>
2021-10-24 10:10:52 +0800

    first commit

---
+++ src/main/java/com/szc/git/Hellogit.java
@@ -0,0 +1,11 @@
+package com.szc.git;
+
+/**
+ * @Author 盛志超
+ * @Date 2021/10/24
+ */
+public class Hellogit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
