c26b3cb6140f3867cfb0935c0d1869b2300474be
szc <iszc@qq.com>
2021-10-24 10:13:40 +0800

    second commit

--- src/main/java/com/szc/git/Hellogit.java
+++ src/main/java/com/szc/git/Hellogit.java
@@ -7,5 +7,7 @@ package com.szc.git;
 public class Hellogit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
     }
 }
